f413bc7f9afcbbdc39b67d9b74ae6e60058bd553
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-30 20:55:59 -0400

    tested database connection capability

--- databasePool.js
+++ databasePool.js
@@ -4,3 +4,12 @@ const databaseConfiguration = require("./secrets/databaseConfiguration.js");
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+// The code below is strictly for debugging and assuring
+// that we are able to query the postgres database
+
+// pool.query("SELECT * FROM generation", (error, response) => {
+//   if (error) return console.log("error", error);
+
+//   console.log("response.rows", response.rows);
+// });
