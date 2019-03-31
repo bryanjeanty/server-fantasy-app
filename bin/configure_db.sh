9dffb1a0e03157a2a4405b6ba5d80efbe5058ab5
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-31 12:49:23 -0400

    created new sql file and reference directories

--- bin/configure_db.sh
+++ bin/configure_db.sh
@@ -5,14 +5,20 @@ echo "Configuring dragonstackdb"
 sudo -u postgres dropdb dragonstackdb
 sudo -u postgres createdb --owner=node_user dragonstackdb
 
-declare -a sqlArr
-for file in ./bin/sql/*.sql 
+declare -a refArr
+for folder in ./bin/sql/*
 do
-    sqlArr=("${sqlArr[@]}" "$file")
-    for item in "${sqlArr[@]}" 
+    refArr=("${refArr[@]}" "$folder")
+done
+
+count=1
+while [ $count -le "${#refArr[@]}" ]
+do
+    for file in ./bin/sql/reference_set_$count/*.sql
     do
-        sudo PGPASSWORD=$DRAGON_PW -u postgres psql -h localhost -U node_user -d dragonstackdb < $item
+        sudo PGPASSWORD=$DRAGON_PW -u postgres psql -h localhost -U node_user -d dragonstackdb < $file
     done
+    let count++
 done
 
 node ./bin/insertTraits.js
