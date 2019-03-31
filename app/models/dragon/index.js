59ce165e1ea535526d95bb966fda607da8fca19d
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-31 18:41:32 -0400

    set up helper function to get dragon with traits

--- app/models/dragon/index.js
+++ app/models/dragon/index.js
@@ -5,6 +5,7 @@ const TRAITS = require("../../../data/traits.json");
 // the lifetime of an application.
 
 const DEFAULT_PROPERTIES = {
+  dragonId: undefined,
   nickname: "unnamed",
   generationId: undefined,
   // We're changing the key-value pair
@@ -38,7 +39,8 @@ const DEFAULT_PROPERTIES = {
 };
 
 class Dragon {
-  constructor({ birthdate, nickname, traits, generationId } = {}) {
+  constructor({ dragonId, birthdate, nickname, traits, generationId } = {}) {
+    this.dragonId = dragonId || DEFAULT_PROPERTIES.dragonId;
     this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
     this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
     this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
