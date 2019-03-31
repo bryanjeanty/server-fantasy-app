5509f1f752a00f062ef63a71d64da1d5445c2dbd
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-30 22:41:02 -0400

    added generationId to dragons

--- app/dragon.js
+++ app/dragon.js
@@ -6,6 +6,7 @@ const TRAITS = require("../data/traits.json");
 
 const DEFAULT_PROPERTIES = {
   nickname: "unnamed",
+  generationId: undefined,
   // We're changing the key-value pair
   // below to a getter pattern
   // birthdate: new Date()
@@ -37,10 +38,11 @@ const DEFAULT_PROPERTIES = {
 };
 
 class Dragon {
-  constructor({ birthdate, nickname, traits } = {}) {
+  constructor({ birthdate, nickname, traits, generationId } = {}) {
     this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
     this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
     this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
+    this.generationId = generationId || DEFAULT_PROPERTIES.generationId;
   }
 }
 
