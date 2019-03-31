41711e71513d601bd94fd0ac88877963d475c5e9
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-30 21:52:17 -0400

    input generation expiration to postgres db and pulled generation id from db to use in api

--- app/generation/table.js
+++ app/generation/table.js
@@ -4,13 +4,19 @@ class GenerationTable {
   // By making the method below static, we can use
   // it without instatiating the class
   static storeGeneration(generation) {
-    pool.query(
-      "INSERT INTO generation(expiration) VALUES($1)",
-      [generation.expiration],
-      (error, response) => {
-        if (error) return console.error(error);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      pool.query(
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id",
+        [generation.expiration],
+        (error, response) => {
+          if (error) return reject(error);
+
+          const generationId = response.rows[0].id;
+
+          resolve({ generationId });
+        }
+      );
+    });
   }
 }
 
