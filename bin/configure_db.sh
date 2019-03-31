48578c6c4e42c607d69de5ce35aff5d31b946cd5
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-03-31 02:26:40 -0400

    created traits table in db

--- bin/configure_db.sh
+++ bin/configure_db.sh
@@ -5,7 +5,16 @@ echo "Configuring dragonstackdb"
 sudo -u postgres dropdb dragonstackdb
 sudo -u postgres createdb --owner=node_user dragonstackdb
 
-sudo -u postgres psql node_user -h localhost -d dragonstackdb < ./bin/sql/generation.sql
-sudo -u postgres psql node_user -h localhost -d dragonstackdb < ./bin/sql/dragon.sql
+declare -a sqlArr
+for file in ./bin/sql/*.sql 
+do
+    sqlArr=("${sqlArr[@]}" "$file")
+    for item in "${sqlArr[@]}" 
+    do
+        sudo PGPASSWORD=$DRAGON_PW -u postgres psql -h localhost -U node_user -d dragonstackdb < $item
+    done
+done
+
+node ./bin/insertTraits.js
 
 echo "dragonstackdb configured"
